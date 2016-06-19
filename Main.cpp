af9e44d58c668ea621e6536a753809fff7f2f60f
Asachun <chuchuchu.chunchun@gmail.com>
2016-06-20 00:06:14 +0900

    Add background.

---
+++ Main.cpp
@@ -0,0 +1,15 @@
+﻿
+# include <Siv3D.hpp>
+
+void Main()
+{
+	Window::Resize(1280, 720);
+
+	Texture background(L"Example/Windmill.png");
+
+	while (System::Update())
+	{
+		// 本来ならリサイズしなくていいように画像を用意すべき.
+		background.scale(1280.0 / 480.0).draw();
+	}
+}
