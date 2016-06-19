da583813deb25f572c920b2d98af326444a7580a
Asachun <chuchuchu.chunchun@gmail.com>
2016-06-20 00:58:27 +0900

    Use Block in Main.

--- Main.cpp
+++ Main.cpp
@@ -40,10 +40,37 @@ void Main()
 	Window::Resize(1280, 720);
 
 	Texture background(L"Example/Windmill.png");
+	Array<Block> blocks;
+
+	blocks.push_back(Block({-400, 400, 200, 200}));
+	blocks.push_back(Block({-200, 400, 200, 200}));
+	blocks.push_back(Block({0, 400, 200, 200}));
+	blocks.push_back(Block({200, 400, 200, 200}));
+	blocks.push_back(Block({200, 200, 200, 200}));
+	blocks.push_back(Block({400, 400, 200, 200}));
+	blocks.push_back(Block({800, 400, 200, 200}));
+	blocks.push_back(Block({1000, 400, 200, 200}));
+	blocks.push_back(Block({1300, 200, 400, 30}));
 
 	while (System::Update())
 	{
+		for (size_t i = 0; i < blocks.size(); i++)
+		{
+			blocks[i].update();
+		}
+
+
+
 		// 本来ならリサイズしなくていいように画像を用意すべき.
 		background.scale(1280.0 / 480.0).draw();
+
+		for (size_t i = 0; i < blocks.size(); i++)
+		{
+			blocks[i].draw();
+		}
+		if (Input::KeyEnter.clicked)
+		{
+			ScreenCapture::Save(L"IMG/a.png");
+		}
 	}
 }
