20ee1b7b0ac4378463aeab83f2e762b5850ef3b0
Asachun <chuchuchu.chunchun@gmail.com>
2016-06-20 00:30:23 +0900

    Add class Block.

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,40 @@
 ﻿
 # include <Siv3D.hpp>
 
+class Block
+{
+public:
+
+	// 引数のないコンストラクタも作っておくといろいろ便利.
+	Block() {}
+
+	Block(const RectF& region) :
+		m_region(region),
+		m_texture(L"Example/Brick.jpg") {}
+
+	// 描画以外の操作をする関数
+	void update()
+	{
+		// 今回は何もない
+	}
+
+	// 描画をする関数（描画操作以外行わないこと.）
+	void draw()
+	{
+		m_region(m_texture).draw();
+	}
+
+
+private:
+
+	// ブロックの領域
+	RectF m_region;
+
+	// ブロックのテキスチャ（画像）
+	Texture m_texture;
+
+};
+
 void Main()
 {
 	Window::Resize(1280, 720);
